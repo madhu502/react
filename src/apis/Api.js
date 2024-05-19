a8014ccbd7586102f8346ed33aa5fb0fed166a0d
Madhu Chaudhary <madhu@Madhus-Laptop.local>
2024-05-19 11:56:45 +0545

    login

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -16,4 +16,7 @@ export const testApi = () => Api.get('/test');
 // Register API
 export const registerUserApi = (data) => Api.post('/api/user/create',data)
 
+//Login API
+export const loginUserApi = (data) => Api.post('/api/user/login', data)
+
 //http://localhost:5500/test
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
+import { loginUserApi } from "../../apis/Api";
 
 const Login =()=>{
     // make a usestate for each input 
@@ -11,10 +12,65 @@ const Login =()=>{
     const [emailError,setEmailError]=useState('')
     const [passwordError,setPasswordError]=useState('')
 
+    // Validation
+    const validation = () => {
+        let isValid = true;
+
+        if (email.trim() === '' || !email.includes ('@')) {
+            setEmailError("Email is empty or invalid")
+            isValid = false;
+            
+        }
+        if (password.trim() === '' ) {
+            setPasswordError("Password is required")
+            isValid = false;
+            
+        }
+        return isValid;
+    }
+
     //Make a function to handle the form submission
     const handleLogin =(e) =>{
         e.preventDefault()
-        toast.success("Login button clicked!")
+        
+        //validation
+        if (!validation()) {
+            return
+        }
+
+        //make a json object
+        const data = {
+            "email":email,
+            "password": password
+
+        }
+        //Make a api request
+        loginUserApi(data).then((res)=>{
+            if (res.data.success === false) {
+                toast.error(res.data.message)
+              }else{
+                toast.success(res.data.message)
+
+                // Success(bool), message(text), token(text) ,user data(json object)
+
+                //Setting token and user data in local storage
+                localStorage.setItem('token',res.data.token)
+
+
+                //Setting user data
+                const convertedData = JSON.stringify(res.data.userData) 
+
+                //local storage set
+                localStorage.setItem('user',convertedData)
+
+              }
+        })
+
+       
+
+
+
+        // toast.success("Login button clicked!")
     }
 
    
@@ -25,9 +81,15 @@ const Login =()=>{
            <form className="w-50"> 
             <label>Email Address {email}</label>
             <input onChange={(e)=> setEmail(e.target.value) } type="text" className="form-control" placeholder="Enter Your email"/>
+            {
+                emailError && <p className="text-danger">{emailError}</p>
+            }
 
             <label>Password {password}</label>
             <input onChange={(e)=> setPassword(e.target.value) } type="password" className="form-control" placeholder="Enter Your password"/>
+            {
+                passwordError && <p className="text-danger">{passwordError}</p>
+            }
 
             <button onClick={handleLogin} className="btn btn-danger mt-2 w-100" >Login</button>
            </form>          
